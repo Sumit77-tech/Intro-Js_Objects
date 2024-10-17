0886470a56e2e12f28104eb38ac5488ee6bf4eb2
SUMIT gourav <sumit.gourav280800@gmail.com>
2024-10-17 15:48:01 -0700

    third commit

---
+++ price.js
@@ -0,0 +1,7 @@
+let price = 750
+let category = (price >= 1000) ? "Expensive" : 
+               (price >= 500 && price <= 999) ? "Moderate" :
+               (price > 0 && price < 500) ? "Affordable" :
+               (price === 0) ? "Free" :
+               "Invalid Price"
+console.log(category)
