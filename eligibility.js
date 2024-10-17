90380dcaa32aedd1956fa328f16cc6369f6d1f8a
SUMIT gourav <sumit.gourav280800@gmail.com>
2024-10-17 14:24:52 -0700

    first commit

---
+++ eligibility.js
@@ -0,0 +1,5 @@
+let age = 54
+let result = (age <= 0) ? "Invalid Age" :
+             (age >= 50) ? "Eligible for Senior Discount" :
+             "Not Eligible for Senior Discount"
+console.log(result)
